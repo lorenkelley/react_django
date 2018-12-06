7cb74b56317c040e76808421985481cb55366102
Loren Kelley <kelley.loren52@gmail.com>
2018-12-06 10:51:54 -0500

    install django project dependencies

--- tunr_react/settings.py
+++ tunr_react/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'tunr_react.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tunr_react_database',
+        'USER': 'tunr_admin',
+        'PASSWORD': 'tunr_admin',
+        'HOST': 'localhost'
     }
 }
 
